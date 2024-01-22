d135618d0134d369dad6997eeee33b9d27269408
Fei <wengangfei@gmail.com>
2024-01-21 21:46:47 -0500

    fnished

--- src/App.js
+++ src/App.js
@@ -17,13 +17,12 @@ data.hits.map(item=><Img_list key={item.id} data={ item }/>): '';
                   word={ word }
                   setData={ setData }
                   setShow={ setShow }/>
-        {show ? <h2>Loading......</h2> : <h2>Start to search.</h2>}
+        {show ? <h1>Loading......</h1> : <h2>Start to search.</h2>}
         <div className="grid grid-cols-4 gap-4">
           { show && display }
         </div>
-      
-      
-      
+        { !show && data && data.hits.length === 0 ? 
+        <h1 className="text-3xl">No images found</h1> : ''}
     </div>
   )
 }
